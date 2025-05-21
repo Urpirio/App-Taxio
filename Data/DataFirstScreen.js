16615a017087fae59415437b37715c0cc6183e50
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-20 21:41:21 -0400

    Se agrego un array de datos para el inicio

---
+++ Data/DataFirstScreen.js
@@ -0,0 +1,21 @@
+import { StyleFirtsScreen } from "../style/StyleFirtsScreen";
+import { StyleSecondScreen } from "../style/StyleSecondScreen";
+import { StyleThirdScreen } from "../style/StyleThirdScreen";
+
+export const DataFirstScreen = [
+    {
+        Img: 'https://static.vecteezy.com/system/resources/previews/063/135/734/non_2x/3d-cartoon-yellow-taxi-car-taxi-vehicle-with-checkered-design-city-transportation-illustration-free-png.png',
+        Description: 'We provide professional taxi services for you',
+        Style: StyleFirtsScreen,
+    },
+    {
+        Img: 'https://static.vecteezy.com/system/resources/previews/060/177/096/non_2x/a-smiling-cartoon-taxi-driver-in-a-dark-blue-uniform-and-cap-welcomes-you-his-friendly-face-and-professional-attire-create-a-positive-and-approachable-image-png.png',
+        Description: 'Your satisfaction is our number one priority',
+        Style: StyleSecondScreen,
+    },
+    {
+        Img: 'https://static.vecteezy.com/system/resources/previews/022/153/704/non_2x/3d-illustration-cute-girls-is-flying-on-a-rocket-free-png.png',
+        Description: `Let't make your day great with Taxio right now!`,
+        Style: StyleThirdScreen,
+    }
+]
